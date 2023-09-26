95c111d3bd170e7d4c78b05ed9d4c75c5de20b64
OMMEENA <om.meena3438@gmail.com>
2023-09-26 10:40:11 +0530

    project

---
+++ README.md
@@ -0,0 +1,13 @@
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### After npm run serve 
+```
+Ctrl + Click http://localhost:8080/ 
---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <task v-bind:tasks="tasks"></task>
+  </div>
+</template>
+
+<script>
+import Task from "./components/Task";
+
+export default {
+  name: "App",
+  components: {
+    Task,
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Go Shopping",
+          completed: true,
+        },
+        {
+          id: 2,
+          title: "Watch Naruto",
+          completed: true,
+        },
+        {
+          id: 3,
+          title: "Learn Python",
+          completed: false,
+        },
+        {
+          id: 4,
+          title: "Learn Guitar",
+          completed: false,
+        },
+        {
+          id: 5,
+          title: "Send Email to Hr",
+          completed: false,
+        },
+      ],
+    };
+  },
+};
+</script>
